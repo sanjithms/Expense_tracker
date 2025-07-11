9386dda5fd21840076ad0e227035c6a0309f6c69
sanjithms <mssanjithsanjithxib@gmail.com@>
2025-07-11 21:56:04 +0530

    edited

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,15 +1,31 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import './Navbar.css';
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleLinkClick = () => setMenuOpen(false);
+
   return (
-    <nav>
-      <h1>Expense Tracker</h1>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/expenses">Expenses</Link>
-        <Link to="/income">Income</Link>
-        <Link to="/history">Transaction History</Link>
-        <Link to="/charts">Chart</Link>
+    <nav className="navbar">
+      <h1>💸 Expense Tracker</h1>
+
+      <div
+        className={`hamburger ${menuOpen ? 'open' : ''}`}
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      <div className={`nav-links ${menuOpen ? 'active' : ''}`}>
+        <Link to="/" onClick={handleLinkClick}>Home</Link>
+        <Link to="/expenses" onClick={handleLinkClick}>Expenses</Link>
+        <Link to="/income" onClick={handleLinkClick}>Income</Link>
+        <Link to="/history" onClick={handleLinkClick}>Transaction History</Link>
+        <Link to="/charts" onClick={handleLinkClick}>Chart</Link>
       </div>
     </nav>
   );
